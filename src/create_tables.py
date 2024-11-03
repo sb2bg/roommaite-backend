2766664c8e0d7fe020c91ac8e0a08718f1a9c288
sb2bg <sullivanbognar@gmail.com>
2024-11-03 01:55:35 -0600

    Add script to create and initialize users table in the database

---
+++ src/create_tables.py
@@ -0,0 +1,23 @@
+import iris
+import json
+
+namespace = "USER"
+port = "1972"
+hostname = "localhost"
+connection_string = f"{hostname}:{port}/{namespace}"
+username = "demo"
+password = "demo"
+
+conn = iris.connect(connection_string, username, password)
+cursor = conn.cursor()
+
+cursor.execute("DROP TABLE data.users")
+cursor.execute(
+    "CREATE TABLE data.users (uuid VARCHAR(255), location VARCHAR(255), answers VECTOR(DOUBLE, 384), prefs VECTOR(DOUBLE, 384))"
+)
+
+# print current db schema
+cursor.execute("SELECT * FROM data.users")
+print(cursor.fetchall())
+
+cursor.close()
